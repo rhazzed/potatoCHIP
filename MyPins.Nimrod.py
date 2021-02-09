e0ea1f54e8a38cea1c68e4848b301507ac2303d1
rhazzed@gmail.com <rhazzed@gmail.com>
2021-02-09 11:57:40 -0600

    changed camera degrees

--- MyPins.Nimrod.py
+++ MyPins.Nimrod.py
@@ -73,7 +73,7 @@ EXIT_DIR_BACKUP_AND_TURN=100
 EXIT_DIR_STUCK=124
 
 # Camera orientation (in degrees)
-CAMERA_ROTATION=0
+CAMERA_ROTATION=180
 
 # COMMAND-PASSING FILE -
 # File to use to pass commands to the robot
